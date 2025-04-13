96eb6e6aba48574d5628abe4b6b7b9b4f9e6f1d7
Nikita <llostinmind@gmail.com>
2025-04-14 03:51:11 +0500

    Update helpers.ts

--- helpers.ts
+++ helpers.ts
@@ -2,5 +2,8 @@ export {
     EventsHandler,
     C3EventsHandler
 } from './helpers/eventsHandler.ts';
-export { C3App } from './helpers/c3App.ts';
+export { 
+    C3App,
+    config
+} from './helpers/c3App.ts';
 export { misc } from './helpers/misc.ts';
